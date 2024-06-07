3b8c6a1387067ad8c506f47405951685e10e29ac
ninicerqueira23 <124396437+ninicerqueira23@users.noreply.github.com>
2024-06-07 09:01:22 -0300

    biblioteca
    biblioteca

---
+++ biblioteca/app.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+
+const app = express();
+const port = 3000;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'ni',
+    password: 'SENAI123',
+    database: 'biblioteca'
+});
+
+db.connect((error) => {
+    if (error) {
+        console.log('Erro ao conectar com o banco de dados');
+    } else {
+        console.log('Conectado ao MYSQL');
+    }
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.post("/login", (req, res) => {
+    const email = req.body.email;
+    const senha = req.body.senha;
+
+    db.query('SELECT senha FROM usuario WHERE email = ?', [email], (error, results) => {
+        if (results.length > 0) {
+            const passwordBD = results[0].senha;
+            if (passwordBD === senha) {
+                console.log('Seja bem-vindo(a)!');
+               
+            } else {
+                console.log('Senha incorreta!');
+               
+            }
+        } else {
+            console.log('Usuário não cadastrado');
+            
+        }
+    });
+});
+
+app.post("/cadastro", (req, res) => {
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const emailConfirm = req.body.emailConfirm;
+    const senha = req.body.senha;
+    const confirm = req.body.senhaConfirm;
+
+    if (email !== emailConfirm) {
+        console.log("Emails não coincidem");
+        
+        return;
+    }
+
+    if (senha !== confirm) {
+        console.log("Senhas não coincidem");
+       
+        return;
+    }
+
+
+    db.query('SELECT email FROM usuario WHERE email = ?', [email], (error, results) => {
+        if (results.length > 0) {
+            console.log("Email já está em uso");
+        } else {
+          
+            db.query('INSERT INTO usuario (nome, email, senha) VALUES (?, ?, ?)', [nome, email, senha], (error, results) => {
+                if (error) {
+                    console.log("Erro ao realizar o cadastro", error);
+                    
+                } else {
+                    console.log("Cadastro realizado com sucesso!");
+                    
+                }
+            });
+        }
+    });
+});
+
+app.get("/cadastro", (req, res) => {
+    res.sendFile(__dirname + '/views/cadastro.html');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando no endereço: http://localhost:${port}`);
+});
